f12342d4f48333454627c316382679d87a02be96
Guilherme Ytalo <gydaytalo@gmail.com>
2023-08-12 15:57:54 -0300

    feat: started styles

--- views/login.view.php
+++ views/login.view.php
@@ -8,8 +8,30 @@
 </head>
 
 <body>
-<h1>login</h1>
+    <div class="flex justify-around text-center	items-center">
+        <div class="w-1/2">
+            <h1 class="text-4xl">Login</h1>
+        </div>
 
+        <form class="w-1/2">
+
+            <div class="mb-2">
+                <label>
+                    Username
+                    <input class="border-2 rounded-full	" type="text" name="username" />
+                </label>
+            </div>
+
+            <div class="mb-2">
+                <label>
+                    Password
+                    <input class="border-2 ml-1 rounded-full	" type="password" name="password" />
+                </label>
+            </div>
+
+            <input type="submit" value="Logar" />
+        </form>
+    </div>
 </body>
 
 </html>
