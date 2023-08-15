77a0cdb6e6675d1fd22191de34da82566a33ea1f
Guilherme Ytalo <gydaytalo@gmail.com>
2023-08-14 23:50:03 -0300

    Started adding database and fix css

--- views/login.view.php
+++ views/login.view.php
@@ -7,20 +7,20 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
-<body>
-    <div>
-        <div>
-            <form class="max-w-md mx-auto p-6 bg-white rounded-lg shadow-md">
-                <div class="mb-4">
-                    <input class="form-input mt-1 block w-full rounded-md border-2 border-solid border-gray-300" type="text" id="username" name="username" placeholder="Username">
-                </div>
-                <div class="mb-4">
-                    <input class="form-input mt-1 block w-full rounded-md border-2 border-solid border-gray-300" type="password" id="password" name="password" placeholder="Password">
-                </div>
-                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">Sign In</button>
-            </form>
-        </div>
+<body class="flex justify-center items-center h-screen">
+<form class="flex items-center flex-col max-w-md mx-auto p-6 bg-white rounded-lg shadow-md">
+    <div class="mb-4">
+        <input class="form-input mt-1 block w-full rounded-md border-2 border-solid border-gray-300 px-2.5" type="text"
+               id="username" name="username" placeholder="Username">
     </div>
+    <div class="mb-4">
+        <input class="form-input mt-1 block w-full rounded-md border-2 border-solid border-gray-300 px-2.5" type="password"
+               id="password" name="password" placeholder="Password">
+    </div>
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg focus:outline-none focus:shadow-outline"
+            type="submit">Sign In
+    </button>
+</form>
 </body>
 
 </html>
